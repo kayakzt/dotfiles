e2e1ddcba6ca43010fe7881d3466ee1c2d2e6b94
kaya <6240107+kayakzt@users.noreply.github.com>
2026-04-13 21:34:14 +0900

    fix(nvim): fix deprecated function

--- nvim/plugins.manager.lua
+++ nvim/plugins.manager.lua
@@ -255,7 +255,7 @@ return {
       -- local disabled_format_clients = { "lua_ls", "volar", "tsserver" }
 
       local on_attach = function(client, bufnr)
-        if client.supports_method("textDocument/formatting") then
+        if client:supports_method("textDocument/formatting") then
           vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
           vim.api.nvim_create_autocmd("BufWritePre", {
             group = augroup,
